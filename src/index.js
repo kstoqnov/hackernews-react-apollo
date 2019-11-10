76df36dcb2667f4dfd0b300099ee3e9bdf50a346
kstoqnov <kirilstoqnov@gmail.com>
2019-11-10 15:38:20 +0200

    add server with prisma work on 4000

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './styles/index.css'
-import App from './components/App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-// 1
-import { ApolloProvider } from 'react-apollo'
-import { ApolloClient } from 'apollo-client'
-import { createHttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
+// 1 add depend
+import { ApolloProvider } from 'react-apollo'; 
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 
-// 2
+// 2 call http link
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
-})
+});
 
-// 3
+// 3 add clinet to server
 const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache()
-})
+});
 
-// 4
+// 4 render 
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>,
   document.getElementById('root')
-)
+);
 serviceWorker.unregister();
